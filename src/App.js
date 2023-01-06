c50dd037e4077ffb39f1f865e5521a2ac139fe61
Marion Pierret <marion.pierretsu@gmail.com>
2023-01-06 11:44:09 +0100

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -7,41 +7,56 @@ function App() {
   const [fiveContacts, setFiveContacts] = useState(Array);
 
   const addRandom = () => {
-      let random = Math.floor(Math.random() * contacts.length)
-      const arrayCopy = [...fiveContacts]
-      arrayCopy.push(contacts[random])
-      setFiveContacts(arrayCopy)
-  }
+    let random = Math.floor(Math.random() * contacts.length);
+    const arrayCopy = [...fiveContacts];
+    arrayCopy.push(contacts[random]);
+    setFiveContacts(arrayCopy)}
+
+  const sortByPopularity = () => {
+    const popularity = [...fiveContacts].sort(function (a, b) {
+      return a.popularity < b.popularity ? 1 : -1;
+    });
+    setFiveContacts(popularity);
+  };
+
+  const sortByName = () => {
+    const nameArray = [...fiveContacts];
+    const name = [...nameArray].sort(function (a, b) {
+      return a.name > b.name ? 1 : -1;
+    });
+    setFiveContacts(name);
+  };
 
   return (
     <div>
-    <h1>IronContacts</h1>
-    <button onClick = {addRandom}>Add Random Contact</button>
-    <button onClick = "">Sort by popularity</button>
-    <button onClick = "">Sort by name</button>
-    <table>
-          <thead>
-            <tr>
-              <th>Picture</th>
-              <th>Name</th>
-              <th>Popularity</th>
-              <th>Won an Oscar</th>
-              <th>Won an Emmy</th>
+      <h1>IronContacts</h1>
+      <button onClick={addRandom}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fiveContacts.map((e) => (
+            <tr key={e.id}>
+              <td>
+                <img src={e.pictureUrl} alt={e.name} />
+              </td>
+              <td>{e.name}</td>
+              <td>{parseFloat(e.popularity).toFixed(2)}</td>
+              {e.wonOscar ? <td>🏆</td> : <td></td>}
+              {e.wonEmmy ? <td>🏆</td> : <td></td>}
             </tr>
-          </thead>
-          <tbody>
-              {fiveContacts.map(e => (              
-                  <tr key={e.id}>
-                    <td><img src={e.pictureUrl} alt={e.name} /></td>
-                    <td>{e.name}</td>
-                    <td>{parseFloat(e.popularity).toFixed(2)}</td>
-                    {e.wonOscar? <td>🏆</td> : <td></td>}
-                    {e.wonEmmy? <td>🏆</td> : <td></td>}
-                  </tr>       
-              ))}    
-          </tbody>
-        </table> 
-
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
